f9135fdd2e3ac958b2cb6126beb80d4986adc7e5
abbgabcal <54662435+abbgabcal@users.noreply.github.com>
2020-03-31 15:41:46 +0200

    Commit

---
+++ driverbot/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+
+      <v-spacer></v-spacer>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon> 
+    </v-app-bar>
+        <v-content>
+      <Controller/>
+    </v-content>
+
+  </v-app>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+import Controller from "./components/Controller";
+
+export default {
+  name: "App",
+
+  components: {
+    Controller
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ driverbot/src/components/Controller.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-container justify-center align-center>
+            <v-card id="ButtonCard" color="blue" align-center>
+            <v-layout row wrap justify-center>
+                <v-flex xs12 md12>
+                    <v-btn id=DirectionStyle>
+                        <v-icon>mdi-chevron-up</v-icon>
+                    </v-btn>
+                </v-flex>
+                <v-flex xs6 md6>
+                    <v-btn id="TurnStyle">
+                        <v-icon>mdi-chevron-left</v-icon>
+                    </v-btn>
+                </v-flex>
+                <v-flex xs6 md6>
+                    <v-btn id="TurnStyle">
+                        <v-icon>mdi-chevron-right</v-icon>
+                    </v-btn>
+                </v-flex>
+                <v-flex xs12 md12>
+                    <v-btn id=DirectionStyle>
+                        <v-icon>mdi-chevron-down</v-icon>
+                    </v-btn>
+                </v-flex>
+            </v-layout>
+        </v-card>
+        
+        
+    </v-container>
+</template>
+
+<script>
+var mqtt = require("mqtt")
+export default {
+    name: "Controller",
+
+    data() {
+        return {
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+/* #directionbutton { */
+    /* size="20px"
+} */
+#ButtonCard {
+    height: 100%;
+    width: 100%;
+}
+#DirectionStyle {
+    margin-left: 40%;
+    width: 20%;
+    height: 100%;
+}
+#TurnStyle {
+    width: 20%;
+    margin-left: 30%;
+}
+</style>
